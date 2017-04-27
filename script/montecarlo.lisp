224741da1b739f529d107cfb30673e73e02b4da3
miumiu0917 <miumiu917@gmail.com>
2017-04-27 01:23:48 +0000

    updated

--- script/montecarlo.lisp
+++ script/montecarlo.lisp
@@ -9,11 +9,16 @@
 (defun one-dot-distance ()
   (distance-from-origin (random-num) (random-num)))
 
-
 (defun less-than-one (l)
   (if (> 1.0 l)
     1.0
       0.0))
 
-(defun do-montecarlo ())
+(defun sum (i)
+  (sum (+ i (less-than-one(one-dot-distance)))))
 
+(defun do-montecarlo ()
+  (loop for i from 1 to *dot-count*
+    do (sum 0)))
+
+(print (do-montecarlo))
